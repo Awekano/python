cf2997b7da9c3c2afd02e2f483845f90807ff10c
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 14:28:48 +0100

    Mise à jour

--- Tuples, Ensembles & Dictionnaires/exercice_convertisseur_unites.py
+++ Tuples, Ensembles & Dictionnaires/exercice_convertisseur_unites.py
@@ -1,3 +1,3 @@
 #En utilisant les tuples, ensembles ou dictionnaire faites un convertisseur d'unité
 #L'utilisateur entrera une valeur en mètres et indiquera ensuite : km, mm ou cm (si il entre une unité autre cela renverra "Unité invalide")
-#Et la convertion se fera par exemple : "Valeur en mètres " : 10, "Valeur souhaité (km, mm, cm) : " km, "Résultat : 0.01km"
+#Et la conversion se fera par exemple : "Valeur en mètres " : 10, "Valeur souhaité (km, mm, cm) : " km, "Résultat : 0.01km"
