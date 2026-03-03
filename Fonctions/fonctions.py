de4e3417d1e70b3b2c17ed8cc83358044cc0591e
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 11:33:59 +0100

    Ajout fonctions.py

---
+++ Fonctions/fonctions.py
@@ -0,0 +1,5 @@
+#Une fonction doit être déclaré içi la fonction hello_world() renvoie "Hello, world !" via la fonction primaire de python print()
+def hello_world():
+    print("Hello, world !")
+
+hello_world()
