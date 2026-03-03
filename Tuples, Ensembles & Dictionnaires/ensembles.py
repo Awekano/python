d11e098506089fec5e3a52c94e4d0d53de7f1d12
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 08:58:58 +0100

    Ajout ensembles.py

---
+++ Tuples, Ensembles & Dictionnaires/ensembles.py
@@ -0,0 +1,6 @@
+#Il n'y a aucune duplication de données dans un ensemble
+mon_ensemble = {"Enzo", "Diego", "Ewan", "Enzo"}
+print(type(mon_ensemble))
+print(len(mon_ensemble))
+print(mon_ensemble)
+#La valeur "Enzo" est représenté 2 fois mais n'est représenté qu'une seule fois
