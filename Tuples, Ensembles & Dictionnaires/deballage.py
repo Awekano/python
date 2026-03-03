44fbd0005e7680174eee29020a325923512917c1
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 09:18:53 +0100

    Ajout deballage.py

---
+++ Tuples, Ensembles & Dictionnaires/deballage.py
@@ -0,0 +1,7 @@
+#Le déballage fonctionne avec les tuples, les ensembles et les listes, il sert à récupérer des valeurs précise 
+loto = 1,15,35,41,23
+liste = ["Angers", "Le Havre", "Brest"]
+l1, l2, l3 = liste
+n1,n2,n3,n4,n5 = loto
+print(n1,n2,n3)
+print(l1,l2,l3)
