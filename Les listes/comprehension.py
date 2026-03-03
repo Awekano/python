e92d825a7c1ba096ad930a526ee5a58a13538df9
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 08:41:45 +0100

    Ajout comprehension.py

---
+++ Les listes/comprehension.py
@@ -0,0 +1,4 @@
+#Les instructions peuvent être intégrés directement dans les listes
+villes = ["angers", "paris", "Marseille", "AnGOulêMe"]
+villes_maj = [v.upper() for v in villes]
+print(villes_maj)
