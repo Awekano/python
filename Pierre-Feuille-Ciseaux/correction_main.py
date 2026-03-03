8f1fc7b9e668a7f341449c36dc4100feca525a2b
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 10:58:40 +0100

    Ajout exercices & correction

--- Pierre-Feuille-Ciseaux/correction_main.py
+++ Pierre-Feuille-Ciseaux/correction_main.py
@@ -7,10 +7,9 @@ choix_utilisateur = input("pierre, feuille ou ciseaux : ")
 
 if choix_utilisateur in options:
     if choix_utilisateur == choix_bot:
-        print("Égalité (^_^;)")
-    if choix_bot == "pierre" and choix_utilisateur == "ciseaux" or choix_bot == "ciseaux" and choix_utilisateur == "feuille" or choix_bot == "feuille" and choix_utilisateur == "pierre":
-        print(f"Dommage tu as perdu le bot à choisi {choix_bot}")
-    elif choix_bot == "pierre" and choix_utilisateur == "feuille" or choix_bot == "ciseaux" and choix_utilisateur == "pierre" or choix_bot == "feuille" and choix_utilisateur == "ciseaux":
-        print(f"Gagné ! {choix_bot}")
-else:
-    print("Choix invalide")
+        print("Égalité !")
+    else:
+        if choix_utilisateur == "pierre" and choix_bot == "ciseaux" or choix_utilisateur == "ciseaux" and choix_bot == "feuille" or choix_utilisateur == "feuille" and choix_bot == "pierre":
+            print(f"Gagné !")
+        else:
+            print(f"Perdu !")
---
+++ Pierre-Feuille-Ciseaux/exercice_main.py
@@ -0,0 +1,5 @@
+#Faire un code qui permet à l'utilisateur de jouer à Pierre-Feuille-Ciseaux face à l'ordinateur (avec égalité possible)
+import random
+
+options = ["pierre", "feuille", "ciseaux"]
+choix_bot = random.choice(options)
