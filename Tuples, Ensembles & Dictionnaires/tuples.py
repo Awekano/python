6b943c294dc50b966876dc4704c251004d465799
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 08:53:07 +0100

    Ajout

---
+++ Tuples, Ensembles & Dictionnaires/tuples.py
@@ -0,0 +1,9 @@
+#Utilisation pour représenter données qui ne change pas (NON MODIFIABLE), deux façon de le créer 
+mon_tuple = (25,10,15)
+mon_tuple_v2 = "France", "Italie", "Espagne", "Royaume-Uni", "Allemagne", "Suisse"
+print(type(mon_tuple))
+print(type(mon_tuple_v2))
+print(mon_tuple[0])
+print(mon_tuple[0:2])
+print(len(mon_tuple))
+#Le tranchage et l'indexage et la fonction len() fonctionnent sur les tuples
