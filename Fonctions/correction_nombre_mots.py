3fcab76d54d2e60a9d98f1f53ff47ce342835d50
Awekano <enzobaylesocheleau@gmail.com>
2026-03-03 16:25:46 +0100

    Mise à jour

--- Fonctions/correction_nombre_mots.py
+++ Fonctions/correction_nombre_mots.py
@@ -1,5 +1,5 @@
-#Définir une fonction qui prendra en arguments une variables qui est une chaîne de caractère
-#Cette fonction retournera une variable qui comptera le nombre de mots dans cette chaîne de caractère
+#Définir une fonction qui prendra en arguments une variables qui est une chaîne de caractère CORRECTION
+#Cette fonction retournera une variable qui comptera le nombre de mots dans cette chaîne de caractère CORRECTION
 def nombre_mots(texte):
     return texte.split(" ")
 
